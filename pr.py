8eca21250857661e9d7cd84ed6600347acd05526
Noobs440 <arieldoubissi330@gmail.com>
2026-07-06 09:48:23 +0100

    kamte

---
+++ pr.py
@@ -0,0 +1,21 @@
+ #POSTGRES_USER=agriconnect
+#POSTGRES_PASSWORD=agriconnectpass
+#POSTGRES_DB=agriconnect_db
+#POSTGRES_PORT=5432
+#REDIS_PORT=6379
+
+#PORT=8000
+#NODE_ENV=development
+#DATABASE_URL=postgresql://agriconnect:agriconnectpass@localhost:5432/agriconnect_db
+#REDIS_URL=redis://localhost:6379
+
+#SMTP_HOST=smtp.gmail.com
+#SMTP_PORT=587
+#SMTP_USER=arieldoubissi4@gmail.com
+#SMTP_PASS=jmzh flmr iykv wqlc
+#SMTP_FROM="AgriConnect <no-reply@agriconnect.com>"
+
+#JWT_SECRET=70873361828e15ecae81bc76cb0bed90385d0c2a0fd9cca198a64ec5c26e883d16674d5eafc254050da34327c21cfbabb05ad766b9415bd6eedafcaf79019d6d
+#JWT_EXPIRES_IN=7d
+#JWT_REFRESH_SECRET=678d20a39c73dd78f6961216b55de24ad4dc1b774d818a6211015abf8cdecdd4e65f141d69ed964547a86a992ce9d5680fd0dbfc91656167a1eb01f9a87c35bb
+#JWT_REFRESH_EXPIRES_IN=30d
